6d42b37b3a262f57d25728c1167aecd9d0cdfc2d
chengshu21 <1981893234@qq.com>
2020-03-26 22:30:33 +0800

    add files

---
+++ global.R
@@ -0,0 +1,22 @@
+library(DT)
+library(shiny)
+library(shinythemes)
+library(hms)
+library(reshape2)
+library(readr)
+library(readxl)
+
+## read refdata into R
+file = "data/GSA Reference DB.xlsx"
+
+## openxlsx::read.xlsx(file,1, startRow=5)
+GTdata <- readxl::read_excel(file, sheet = 1, range = "A5:L355")
+RBdata <- readxl::read_excel(file, sheet = 2,  range = "A5:G66")
+GeGdata <- readxl::read_excel(file, sheet = 3, range = "A5:H66")
+ncGdata <- readxl::read_excel(file, sheet = 4, range = "A5:H35")
+
+
+## analysis
+source("R/2019_Mora_Popularity_versus_Performance.R")
+
+
---
+++ ui.R
@@ -0,0 +1,38 @@
+shinyUI(fluidPage(
+  # includeScript(path = "www/try.js"),
+  navbarPage(
+    title=div(img(src="GSACENTRAL_Logo.png", width = "100px", height = "50px")),
+    # h4("GSA-ref-DB"),
+    # inverse = TRUE,
+    tags$hr(),
+    ##-- Title Header ----
+    tags$div(
+      class = "text-center",
+      span(strong(h2("GSARefDB - The Gene Set Analysis Reference Database")),
+           style = "color:steelblue")),
+    tags$hr(),
+    tabPanel(strong(h4("General Methods and Tools")),
+             # Download Button
+             # downloadButton('downloadData', 'Download'),
+             DT::dataTableOutput("DT1")),
+    tabPanel(h4("Reviews and Benchmarks"),
+             DT::dataTableOutput("DT2")),
+    tabPanel(h4("Genomic GSA"),
+            DT::dataTableOutput("DT3")),
+    tabPanel(h4("ncRNA GSA"),
+             DT::dataTableOutput("DT4")),
+    tabPanel(h4("Analysis"),
+             shiny::plotOutput("plot",width = "100%", height = "1000px")),
+    tabPanel(h4("Submit"),
+             includeMarkdown("www/files/Submit.md")),
+    tabPanel(h4("Archive"), 
+             includeMarkdown("www/files/Archive.md")),
+    tabPanel(h4("FAQs"), 
+             includeMarkdown("www/files/FAQs.md")),
+    selected = strong(h4("General Methods and Tools")),
+    tags$hr(),
+    footer = tags$div(
+      class = "text-center",
+      span(strong(h3("Last updated: March 16th, 2020")),
+           style = "color:gray")))
+))
